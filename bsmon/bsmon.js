d49c9961f37bdddfe2bdd09d54db9fe9c1ff6378
Davide Kirchner <davide.kirchner@yahoo.it>
2014-08-03 12:47:45 +0200

    Server and client events API and server status API, with viewing template skeleton

--- bsmon/bsmon.js
+++ bsmon/bsmon.js
@@ -1,10 +1,30 @@
-CliEvents = new Meteor.Collection("cli_events");
-
+ClientEvents = new Meteor.Collection("cli_events");
+ServerEvents = new Meteor.Collection("server_events");
+ServerStatus = new Meteor.Collection("server_status");
 
 if (Meteor.isClient) {
-    Template.client.events = function () {
-        //return "Welcome to bsmon.";
-        return CliEvents.find({}, {});
+    Template.client.evt_list = function () {
+        return ClientEvents.find({}, {sort: {date: 1}});
+    };
+    Template.server.evt_list = function () {
+        return ServerEvents.find({}, {sort: {date: 1}});
+    };
+    Template.server.status = function () {
+        var st =  ServerStatus.findOne({});
+        if (typeof st !== 'undefined') {
+            // just an example to start working on the UI
+            st.parsed_db = [
+                {
+                    index: 0,
+                    ip: '123.2.2.2'
+                },
+                {
+                    index: 1,
+                    ip: '123.2.2.2'
+                },
+            ];
+        }
+        return st;
     };
 
     Template.client.events({
@@ -18,13 +38,29 @@ if (Meteor.isClient) {
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
-        //CliEvents.remove({});
+        //ServerStatus.remove({});
+        //ServerEvents.remove({});
         // code to run on server at startup
         collectionApi = new CollectionAPI({
             apiPath: 'rest',
         });
 
-        collectionApi.addCollection(CliEvents, 'client', {});
+        collectionApi.addCollection(ClientEvents, 'client-events', {
+            methods: ['POST'], //methods: ['POST','GET','PUT','DELETE'],
+        });
+        collectionApi.addCollection(ServerEvents, 'server-events', {
+            methods: ['POST'],
+        });
+        collectionApi.addCollection(ServerStatus, 'server-status', {
+            methods: ['POST'],
+            before: {
+                POST: function (obj) {
+                    // manage ServerStatus as a 1-object collection
+                    ServerStatus.remove({});
+                    return true;  // will now perform insertion normally
+                }
+            }
+        });
 
         collectionApi.start();
     });
